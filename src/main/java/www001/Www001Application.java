50eb0557bd65af6c6e19a039ab13403c39aa4c4c
Matt Cadorette <mcadorette@gmail.com>
2018-10-18 11:34:19 -0400

    Initial commit from Atomist

---
+++ src/main/java/www001/Www001Application.java
@@ -0,0 +1,12 @@
+package www001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Www001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Www001Application.class, args);
+	}
+}
---
+++ src/test/java/www001/Www001ApplicationTests.java
@@ -0,0 +1,16 @@
+package www001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Www001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
